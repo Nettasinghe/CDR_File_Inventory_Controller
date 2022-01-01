8757a7bfe9e6ccba27f10efbaae1bac5eb9dc7dd
Chandika Nettasinghe <93026500+Nettasinghe@users.noreply.github.com>
2022-01-01 10:31:53 +0530

    Update Main.C

--- Main.C
+++ Main.C
@@ -5,9 +5,9 @@
 // CDR File Inventory Controller -
 // The functionality of the CDR File Inventory Controller is to transfer the CDR files from the 
 // Telco Switches (MSC, SMSC etc where the CDR files are generated) to the Billing System locations
-// to be processed my the Switch Mediation Engines. The Token process will make sure that only one 
-// instance of this will be up and running at a perticular time. ie. it prevents the multiple deamons
-// of this, running. Aditionally CDR file info is logged to the database so these can be processed by 
+// to be processed by the Switch Mediation Engines. The Token process will make sure that only one 
+// instance of this will be up and running at a particular time. ie. it prevents the multiple deamons
+// of this, running. Additionally, CDR file info is logged to the database so these can be processed by 
 // the relevant Mediation Engines.
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////
